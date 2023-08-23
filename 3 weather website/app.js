4ee68944aac7b5e8924f2d6c907a3002ee108ee3
raamiiChu <ericchu0617@gmail.com>
2023-08-23 17:12:38 +0800

    Update: weather website

---
+++ 3 weather website/app.js
@@ -0,0 +1,53 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+import ejs from "ejs";
+import fetch from "node-fetch";
+
+// api key
+const KEY = "4a8df43351b52806cbd9c10217846929";
+
+// kelvin to celsius
+function kelvinToCelsius(k) {
+    return Math.round((k - 272.15) * 10) / 10;
+}
+
+// middleware
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.get("/:city", async (req, res) => {
+    let { city } = req.params;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${KEY}`;
+
+    // get response by node-fetch
+    let data = await fetch(url);
+    data = await data.json();
+
+    // kelvin to celsius
+    let { temp } = data.main;
+    let temperature = kelvinToCelsius(temp);
+
+    res.render("weather.ejs", { data, temperature });
+
+    // 另一種寫法
+    // fetch(url)
+    //     .then((res) => {
+    //         return res.json();
+    //     })
+    //     .then((data) => {
+    //         let { temp } = data.main;
+    //         let temperature = kelvinToCelsius(temp);
+
+    //         res.render("weather.ejs", { data, temperature });
+    //     });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}!`);
+});
